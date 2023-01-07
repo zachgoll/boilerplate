68152c4183f8ff9213cc57f475a1aeeec1899c34
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2023-01-07 14:33:05 -0500

    Final template vanilla react 18 TS

---
+++ vanilla-react18-typescript-webpack/src/App.tsx
@@ -0,0 +1,10 @@
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main>
+      <p>App</p>
+      <Footer />
+    </main>
+  );
+}
---
+++ vanilla-react18-typescript-webpack/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="#">A Footer Link</a>
+    </footer>
+  );
+}
